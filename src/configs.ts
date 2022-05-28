bf3f003f8241175744ab89a971b6261728390328
fmukaba <mukaba.francois@bcg.com>
2022-05-27 23:51:04 -0700

    using firebase -try firestore

---
+++ src/configs.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQn07UXKIDSX7x8RiZBkpeJGbIKS4ydQ0",
+    authDomain: "reflectionblog-c3156.firebaseapp.com",
+    projectId: "reflectionblog-c3156",
+    storageBucket: "reflectionblog-c3156.appspot.com",
+    messagingSenderId: "413575259629",
+    appId: "1:413575259629:web:07fc3186d3ef42b23973e4",
+    measurementId: "G-MNVWLT5HDD"
+  };
+
+  export default firebaseConfig;
--- src/pages/Article/Articles.tsx
+++ src/pages/Article/Articles.tsx
@@ -1,6 +1,19 @@
-import { FC } from "react";
+import { collection, getDocs } from "@firebase/firestore";
+import { FC, useEffect } from "react";
+import db from "../../firebase";
 
 const Article: FC = () => {
+  const articlesCollectionRef = collection(db, "articles");
+  useEffect(() => {
+    console.log("started");
+    const getArticles = async () => {
+      const data = await getDocs(articlesCollectionRef);
+      console.log(data);
+    };
+
+    getArticles();
+  });
+
   return <div> List of all articles </div>;
 };
 
